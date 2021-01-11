cc29230e020626d6ef6faa8af2a9618ff054e4ae
Andre Biel <biermitl@gmail.com>
2021-01-11 09:35:03 +0100

    Add nvim plugins

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Install Fuzzy search engine and useful keybindings
+brew install fzf
+$(brew --prefix)/opt/fzf/install
+
+# Install color highlighting and command line previews
+brew install bat
+
 # Link zsh
 rm ~/.zshrc
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.nvim/bundle/Vundle.vi
 
 # Link neovim
 rm -rf ~/.config/nvim

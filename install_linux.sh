eb6611baf9a2874c8e1ba30b6fd891ae0c350ea2
Andre Biel <biermitl@gmail.com>
2021-04-19 21:46:46 +0200

    Add stuff to stuff

--- install_linux.sh
+++ install_linux.sh
@@ -12,5 +12,11 @@ rm -rf ~/.config/alacritty
 mkdir -p ~/.config/alacritty
 ln -sf ~/dotfiles/alacritty/alacritty_linux.yml ~/.config/alacritty/alacritty.yml
 
+# i3
+rm -rf ~/.config/i3
+mkdir -p ~/.config/i3
+ln -sf ~/dotfiles/i3/config ~/.config/i3/config
+
+
 
 
